d6bee740a0c38d79f35d184e21bcdd7efeb1b712
Matthew Lam <lamma@engr.orst.edu>
2023-11-07 18:03:53 -0500

    changed code to only download the first video

--- main.py
+++ main.py
@@ -7,17 +7,22 @@ PATH = 'Downloaded_Videos'
 if not os.path.exists(PATH):
     os.makedirs(PATH)
 
-url = 'https://www.youtube.com/watch?v=9bZkp7q19f0&ab_channel=officialpsy'
+# ONLY CHANGE THIS URL
+url = 'https://www.youtube.com/watch?v=mYDBDd3N_IE&ab_channel=Lulunolly'
+
 yt = YouTube(url)
+yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(output_path='Downloaded_Videos')
+
+#  --------------------USE CODE BELOW ONLY FOR SPECIFIC VIDEO SELECTING ----------------------------
 
 # Used to see what versions of the video that can be downloaded
-for stream in yt.streams.filter(file_extension='mp4').filter(type='video'):
-    print(stream)
+# for stream in yt.streams.filter(file_extension='mp4').filter(type='video'):
+#     print(stream)
 
 # Change the itag value to the one you want to download
 # Comment the code out below so it doesn't run when you looking for video's itag
 
-itag = 18
-stream = yt.streams.get_by_itag(itag)
-stream.download(output_path='Downloaded_Videos')
+# itag = 18
+# stream = yt.streams.get_by_itag(itag)
+# stream.download(output_path='Downloaded_Videos')
 print('Finished Downloading')
